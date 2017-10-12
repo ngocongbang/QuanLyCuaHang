305767270ad0642276f9a1e450626120c28a9f76
Xuan An <xuanan0209@gmail.com>
2017-10-12 09:05:14 +0700

    Tạo file VenderController.cs

---
+++ ManagementStore/ManagementStore/Controllers/VenderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagementStore.Controllers
+{
+    public class VenderController : Controller
+    {
+        // GET: Vender
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
