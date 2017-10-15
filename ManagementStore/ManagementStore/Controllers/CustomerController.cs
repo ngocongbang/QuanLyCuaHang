af5fb662b75ead7c4c00318b39a541c0f61ca037
bangnc\bangnctv <bang.ngo>
2017-10-15 17:29:30 +0700

    phan trang

--- ManagementStore/ManagementStore/Controllers/CustomerController.cs
+++ ManagementStore/ManagementStore/Controllers/CustomerController.cs
@@ -1,6 +1,8 @@
 ﻿using ManagementStore.Business.Common.Constants;
 using ManagementStore.Business.Customers;
+using ManagementStore.Models;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace ManagementStore.Controllers
 {
@@ -9,9 +11,12 @@ namespace ManagementStore.Controllers
     {
         CustomerHandler _customerHandler = new CustomerHandler();
         // GET: Customers
-        public ActionResult Index(int? pageSize, int? pageCurrent)
+        //[HttpGet]
+        public ActionResult Index()
         {
-            if (pageSize ==null)
+            int? pageSize = null;
+            int? pageCurrent = null;
+            if (pageSize == null)
             {
                 pageSize = MessageResConst.PageSize;
             }
@@ -20,7 +25,45 @@ namespace ManagementStore.Controllers
                 pageCurrent = 1;
             }
             var listCustomer = _customerHandler.GetCustomers((int)pageSize, (int)pageCurrent);
-            return View(listCustomer.Data);
+            CustomerViewModel viewModel = new CustomerViewModel();
+            viewModel.ListCustomerModel = listCustomer.Data;
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Index(int? pageSize, int? pageCurrent, CustomerModel customerModel)
+        {
+            if (pageSize == null)
+            {
+                pageSize = MessageResConst.PageSize;
+            }
+            if (pageCurrent == null)
+            {
+                pageCurrent = 1;
+            }
+            var listCustomer = _customerHandler.GetCustomers((int)pageSize, (int)pageCurrent);
+            CustomerViewModel viewModel = new CustomerViewModel();
+            viewModel.ListCustomerModel = listCustomer.Data;           
+            return View(viewModel);
+
+
+        }
+        [HttpPost]
+        public JsonResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
+                //if (result.Succeeded)
+                //{
+                //     //return RedirectToLocal(returnUrl);
+                //}
+
+                ModelState.AddModelError("", "Identifiant ou mot de passe invalide");
+                return Json("error-model-wrong");
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Json("error-mode-not-valid");
         }
     }
 }
