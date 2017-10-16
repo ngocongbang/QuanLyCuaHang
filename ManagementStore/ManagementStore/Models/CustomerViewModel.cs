49c5d49ff9b843ccf1411d37c860e4581a5de468
bangnc\bangnctv <bang.ngo>
2017-10-16 20:26:22 +0700

    add

---
+++ ManagementStore/ManagementStore/Models/CustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ManagementStore.Business.Customers;
+
+namespace ManagementStore.Models
+{
+    public class CustomerViewModel
+    {
+        public IEnumerable<CustomerModel> ListCustomerModel { get; set; }
+        public CustomerModel CustomerModel { get; set; }
+    }
+}
