ef080f1cffa416bce017550c7214d63929b866ef
Xuan An <xuanan0209@gmail.com>
2017-10-30 16:52:22 +0700

    tạo mới hàng hóa

---
+++ ManagementStore/ManagementStore/Controllers/ItemController.cs
@@ -0,0 +1,58 @@
+﻿using ManagementStore.Business.Common.Constants;
+using ManagementStore.Business.Items;
+using ManagementStore.Models;
+using ManagementStore.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagementStore.Controllers
+{
+    public class ItemController : Controller
+    {
+        ItemHandler itemHander = new ItemHandler();
+        // GET: Item
+        public ActionResult Index()
+        {
+            int? pageSize = null;
+            int? pageCurrent = null;
+            if (pageSize == null)
+            {
+                pageSize = MessageResConst.PageSize;
+            }
+            if (pageCurrent == null)
+            {
+                pageCurrent = 1;
+            }
+            var lstItem = itemHander.GetItems((int)pageSize, (int)pageCurrent, "Name", "increase", null);
+            ItemViewModel viewModel = new ItemViewModel();
+            viewModel.ListItemModel = lstItem.Data;
+            int size = lstItem.CountData / (int)pageSize + 1;
+            viewModel.DisplayPage = pageCurrent.ToString() + "/" + size.ToString();
+            viewModel.CountPage = size;
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Index(int? pageSize,int? pageCurrent,string column,string orderASCorDSC, ItemModel itemModel)
+        {
+            if (pageSize == null)
+            {
+                pageSize = MessageResConst.PageSize;
+            }
+            if (pageCurrent == null)
+            {
+                pageCurrent = 1;
+            }
+            ViewBag.PageSize = ListPageSize.GetListPageSize();
+            var listItem = itemHander.GetItems((int)pageSize, (int)pageCurrent, column, orderASCorDSC, itemModel);
+            ItemViewModel viewModel = new ItemViewModel();
+            viewModel.ListItemModel = listItem.Data;
+            int size = listItem.CountData / (int)pageSize + 1;
+            viewModel.DisplayPage = pageCurrent.ToString() + "/" + size.ToString();
+            viewModel.CountPage = size;
+            return View(viewModel);
+        }
+    }
+}
---
+++ ManagementStore/ManagementStore/Models/ItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ManagementStore.Business.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagementStore.Models
+{
+    public class ItemViewModel
+    {
+        public IEnumerable<ItemModel> ListItemModel { get; set; }
+        public string DisplayPage { get; set; }
+        public int CountPage { get; set; }
+        public ItemModel ItemModel { get; set; }
+    }
+}
