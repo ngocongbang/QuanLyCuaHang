de65ef9fb1d31a3406d34f6f09265cbbb9b2b832
bangnc <bangnc@ominext.com>
2017-10-31 08:49:29 +0700

    Item_ColorViewModel

---
+++ ManagementStore/ManagementStore/Models/Item_ColorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ManagementStore.Business.Item_Colors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagementStore.Models
+{
+    public class Item_ColorViewModel
+    {
+        public IEnumerable<Item_ColorModel> ListItem_ColorModel { get; set; }
+        public string DisplayPage { get; set; }
+        public int CountPage { get; set; }
+        public Item_ColorModel Item_ColorModel { get; set; }
+    }
+}
