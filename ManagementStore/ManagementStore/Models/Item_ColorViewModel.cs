20aae835391f3b675f469de6a2121eb6fdfd3796
bangnc <bangnc@ominext.com>
2017-10-31 09:20:32 +0700

    Hàng hóa - Kích thước

--- ManagementStore/ManagementStore/Models/Item_ColorViewModel.cs
+++ ManagementStore/ManagementStore/Models/Item_ColorViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using ManagementStore.Business.Item_Colors;
+using ManagementStore.Business.Item_Materials;
+using ManagementStore.Business.Item_Sizes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +14,19 @@ namespace ManagementStore.Models
         public string DisplayPage { get; set; }
         public int CountPage { get; set; }
         public Item_ColorModel Item_ColorModel { get; set; }
+    }
+    public class Item_SizeViewModel
+    {
+        public IEnumerable<Item_SizeModel> ListItem_SizeModel { get; set; }
+        public string DisplayPage { get; set; }
+        public int CountPage { get; set; }
+        public Item_SizeModel Item_SizeModel { get; set; }
+    }
+    public class Item_MaterialViewModel
+    {
+        public IEnumerable<Item_MaterialModel> ListItem_MaterialModel { get; set; }
+        public string DisplayPage { get; set; }
+        public int CountPage { get; set; }
+        public Item_MaterialModel Item_MaterialModel { get; set; }
     }
 }
