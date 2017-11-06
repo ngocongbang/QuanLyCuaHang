ee06fb137f5df09f030cb4d8ceb85deafd6146bb
bangnc\bangnctv <bang.ngo>
2017-11-06 19:30:04 +0700

    fff

--- ManagementStore/ManagementStore.Business/Branchs/BranchModel.cs
+++ ManagementStore/ManagementStore.Business/Branchs/BranchModel.cs
@@ -12,7 +12,7 @@ namespace ManagementStore.Business.Branchs
         public int Branch_ID { get; set; }
         public string Branch_Name { get; set; }
         public string Region { get; set; }
-        public string CommuneWard { get; set; }
+        public string CommuneWard { get; set;  }
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
